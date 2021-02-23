c9bb44fe115d3a0545d5cba67c946af2bd138fff
Nariyuki Saito <toshincompos@gmail.com>
2021-02-23 23:35:58 +0900

    remove unnecessary functions in controller utils

--- src/controllers/util.ts
+++ src/controllers/util.ts
@@ -4,24 +4,7 @@ import {
   ResourceConflictValidationError,
 } from 'src/errors/validationErrors';
 
-import { InvalidInputFormatValidationError } from './errors';
-import { Request } from './types/request';
 import { Response } from './types/response';
-
-export function validateRequest(
-  request: Request,
-  assertFunc: (request: Request) => void
-): Response | void {
-  try {
-    assertFunc(request);
-  } catch (error) {
-    if (error instanceof InvalidInputFormatValidationError) {
-      return buildErrorResponse(error);
-    } else {
-      throw error;
-    }
-  }
-}
 
 export function buildErrorResponse(error: Error): Response {
   if (error instanceof InvalidInputValidationError) {
